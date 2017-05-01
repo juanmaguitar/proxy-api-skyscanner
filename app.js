e0465795be6a4792a1df3b3e769e6c9cef0ac6d0
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2017-05-01 17:02:51 +0200

    proxy api skyscanner

---
+++ app.js
@@ -0,0 +1,26 @@
+const request = require('request')
+const cors = require('cors')
+const express = require('express')
+const PORT = process.env.PORT || 3000
+const app = express()
+
+const URL_BASE = 'http://partners.api.skyscanner.net/apiservices/reference/v1.0'
+
+const options = {
+  headers: {
+    'Accept': 'application/json'
+  }
+}
+
+app.use(cors())
+
+app.get('*', function (req, res) {
+  options.url = URL_BASE + req.originalUrl + '?apiKey=' + process.env.API_KEY
+  request(options, function (error, response, body) {
+    if (error) throw error
+    res.json(JSON.parse(body))
+  })
+})
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}...`))
+
