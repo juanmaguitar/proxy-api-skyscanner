3bc46dc85ace0b8ecbcbce4dc5c201295ac95042
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2017-05-01 17:10:32 +0200

    fixed

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.get('*', function (req, res) {
   options.url = URL_BASE + req.originalUrl + '?apiKey=' + process.env.API_KEY
   request(options, function (error, response, body) {
     if (error) throw error
-    res.json(JSON.parse(body))
+    res.json(body)
   })
 })
 
